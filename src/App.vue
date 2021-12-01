a49b5f86513512ff91beee9ea1ebda67eac7bfb8
tarjanovri <sistema1>
2021-12-02 00:49:39 +0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
-  <PrimeLine/>
-
+  <sidebar-menu :menu="menu"/>
+    <div id="nav">
+    <router-link to="/charts/primeline">Home</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <script>
-import PrimeLine from "@/components/PrimeLine";
+import {SidebarMenu} from 'vue-sidebar-menu'
+
 
 export default {
   name: 'app',
   components: {
-    PrimeLine,
+    SidebarMenu
   },
+  data(){
+    return{
+        menu: [
+          {
+            header: 'Main Navigation',
+            hiddenOnCollapse: true
+          },
+          {
+            href: '/',
+            title: 'Dashboard',
+            icon: 'fa fa-user'
+          },
+          {
+            href: '/charts',
+            title: 'Charts',
+            icon: 'fa fa-chart-area',
+            child: [
+              {
+                href: '/charts/primeline',
+                title: 'PrimeLine'
+              }
+            ]
+          }
+        ]
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import { createRouter, createWebHistory } from 'vue-router'
+import PrimeLine from "./components/PrimeLine";
 
-createApp(App).mount('#app')
+
+const routes = [
+  {
+    path: '/charts/primeline',
+    name: 'PrimeLine Chart',
+    component: PrimeLine
+  }
+]
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+
+const app = createApp(App)
+app.use(router)
+app.use(VueSidebarMenu)
+app.mount('#app')
